ed32a29e5423abb4c036de575c2c2b00708a216b
Heiru Wu <wuheiru.5203@gmail.com>
2014-07-03 14:26:30 +0800

    Open eyny on default browser

---
+++ src/something/DesktopRobo.java
@@ -0,0 +1,33 @@
+package something;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class DesktopRobo {
+	URI uri;
+	public DesktopRobo() {
+		// TODO Auto-generated constructor stub
+		try {
+			uri = new URI("http://www.eyny.com");
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public DesktopRobo(URI uri){
+		this.uri = uri;
+	}
+	void OpenUrl() throws URISyntaxException{
+		if(Desktop.isDesktopSupported())
+		{
+		  try {
+			Desktop.getDesktop().browse(uri);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}
+	}
+}
--- src/something/Practice.java
+++ src/something/Practice.java
@@ -1,10 +1,21 @@
 package something;
 
+import java.net.URISyntaxException;
+
+
 public class Practice {
+	static DesktopRobo mDesktopRobo;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Project initialized");
+		mDesktopRobo = new DesktopRobo();
+		System.out.print("Application initialized");
+		try {
+			mDesktopRobo.OpenUrl();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
