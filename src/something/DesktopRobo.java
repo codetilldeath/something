94027feebced137dc5c1b4dc0d6746255a8dd7d0
Heiru Wu <wuheiru.5203@gmail.com>
2014-07-08 17:16:54 +0800

    Full function of Ping and ifconfig

--- src/something/DesktopRobo.java
+++ src/something/DesktopRobo.java
@@ -1,7 +1,9 @@
 package something;
 
 import java.awt.Desktop;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.net.CookiePolicy;
@@ -13,26 +15,28 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
 
-
 /**
  * Virtual robot for desktop helping
  */
 public class DesktopRobo {
+	String strUrl;
 	URI uri;
 	URL url;
 	URLConnection mURLConnection;
 	CookieManager mCookieManager;
 	Object mObject;
 	CookieStore mCookieStore;
+
 	/**
-	 * Construct Dektop Robot Object.
-	 * Construct with input URL.
-	 * @throws IOException 
+	 * Construct Dektop Robot Object. Construct with input URL.
+	 * 
+	 * @throws IOException
 	 */
 	public DesktopRobo(String url) throws IOException {
 		// TODO Auto-generated constructor stub
+		strUrl = url.substring(7);
 		try {
-			this.uri = new URI("http://www.eyny.com");
+			this.uri = new URI(url);
 			this.url = new URL(url);
 			this.mCookieManager = new CookieManager();
 			mCookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
@@ -42,40 +46,75 @@ public class DesktopRobo {
 			e.printStackTrace();
 		}
 	}
+
 	/**
 	 * Custom constructor with url
 	 */
-	public DesktopRobo(URI uri){
+	public DesktopRobo(URI uri) {
 		this.uri = uri;
 	}
+
 	/**
 	 * Execute browsing url
 	 */
-	void OpenUrl() throws URISyntaxException{
-		if(Desktop.isDesktopSupported())
-		{
-		  try {
-			Desktop.getDesktop().browse(uri);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			System.out.println("Error browsing url");
-		}
+	void OpenUrl() throws URISyntaxException {
+		if (Desktop.isDesktopSupported()) {
+			try {
+				Desktop.getDesktop().browse(uri);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Error browsing url");
+			}
 		}
 	}
+
 	/**
 	 * Get cookies
 	 */
-	public void GetCookie() throws IOException{
+	public void GetCookie() throws IOException {
 		mURLConnection = url.openConnection();
 		mObject = mURLConnection.getContent();
 		mCookieStore = mCookieManager.getCookieStore();
 		List<HttpCookie> cookies = mCookieStore.getCookies();
-	    for (HttpCookie cookie : cookies) {
-	    	System.out.println("Got cookies");
-	    	System.out.println(cookie.getName());
-//	    	System.out.println(cookie.getComment());
-//	    	System.out.println(cookie.getValue());
-	    	System.out.println(cookie);
-	    }
+		for (HttpCookie cookie : cookies) {
+			System.out.println("Got cookies");
+			System.out.println("Name: " + cookie.getName());
+			System.out.println("Domain: " + cookie.getDomain());
+			// System.out.println(cookie.getComment());
+			// System.out.println(cookie.getValue());
+			System.out.println("Value: " + cookie);
+		}
+	}
+	/**
+	 * Ping particular ip address
+	 * @throws IOException 
+	 * @throws InterruptedException 
+	 */
+	public void PingIP() throws IOException, InterruptedException{
+		String command = "ping -c 3 " + strUrl;
+        Process proc = Runtime.getRuntime().exec(command);
+        BufferedReader reader = 
+            new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        String line = "";
+        while((line = reader.readLine()) != null) {
+            System.out.print(line + "\n");
+        }
+        proc.waitFor();
+	}
+	/**
+	 * Get local IP address
+	 * @throws IOException 
+	 * @throws InterruptedException 
+	 */
+	public void IpStat() throws IOException, InterruptedException{
+		String command = "ifconfig";
+		Process proc = Runtime.getRuntime().exec(command);
+        BufferedReader reader = 
+            new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        String line = "";
+        while((line = reader.readLine()) != null) {
+            System.out.print(line + "\n");
+        }
+        proc.waitFor();
 	}
 }
--- src/something/Practice.java
+++ src/something/Practice.java
@@ -3,19 +3,33 @@ package something;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URISyntaxException;
-
 
 public class Practice {
 	static DesktopRobo mDesktopRobo;
 
-	public static void main(String[] args) throws IOException, URISyntaxException {
+	public static void main(String[] args) throws IOException {
 		String read;
 		System.out.println("Application initialized");
+		Runtime.getRuntime().exec("/bin/bash -c ifconfig");
 		System.out.println("Enter URL:");
-		read = "http://"+new BufferedReader(new InputStreamReader(System.in)).readLine();
+		read = "http://"
+				+ new BufferedReader(new InputStreamReader(System.in))
+						.readLine();
 		mDesktopRobo = new DesktopRobo(read);
 		mDesktopRobo.GetCookie();
-	}
+		new Thread(new Runnable() {
 
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				try {
+					mDesktopRobo.IpStat();
+					mDesktopRobo.PingIP();
+				} catch (IOException | InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}).start();
+	}
 }
