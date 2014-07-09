05caf082eaae12f4b745054b9d430c3648928059
Heiru Wu <wuheiru.5203@gmail.com>
2014-07-09 15:03:08 +0800

    Little fix

--- src/something/Practice.java
+++ src/something/Practice.java
@@ -6,30 +6,31 @@ import java.io.InputStreamReader;
 
 public class Practice {
 	static DesktopRobo mDesktopRobo;
+	static Thread intTest;
+
+	static Runnable connect = new Runnable() {
+		public void run() {
+			// TODO Auto-generated method stub
+			try {
+				mDesktopRobo.IpStat();
+				mDesktopRobo.PingIP();
+				mDesktopRobo.GetCookie();
+			} catch (IOException | InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	};
 
 	public static void main(String[] args) throws IOException {
 		String read;
 		System.out.println("Application initialized");
-		Runtime.getRuntime().exec("/bin/bash -c ifconfig");
 		System.out.println("Enter URL:");
 		read = "http://"
 				+ new BufferedReader(new InputStreamReader(System.in))
 						.readLine();
 		mDesktopRobo = new DesktopRobo(read);
-		mDesktopRobo.GetCookie();
-		new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				// TODO Auto-generated method stub
-				try {
-					mDesktopRobo.IpStat();
-					mDesktopRobo.PingIP();
-				} catch (IOException | InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}).start();
+		intTest = new Thread(connect);
+		intTest.start();
 	}
 }
